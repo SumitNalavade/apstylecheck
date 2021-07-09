e372bfcde02872984b586cff95291910c211d9f1
Sumit Nalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-07-09 18:58:14 -0500

    make form

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,4 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
