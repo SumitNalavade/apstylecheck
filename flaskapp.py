4b92b806e839f4110a69d93a44fcdf25b9d55e59
Sumit Nalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-07-11 02:05:49 -0500

    redesign page

--- flaskapp.py
+++ flaskapp.py
@@ -2,8 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/home")
 @app.route("/")
 def home_page():
     return render_template("index.html")
 
+
+@app.route('/send', methods=['POST'])
+def send():
+    if(request.method == 'POST'):
+        userText = request.form['usertext']
+
+    print(userText)
+
+    return(render_template('index.html'))
