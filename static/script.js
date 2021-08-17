b0db3b4346eb7a340cbdfa80a451eeaedcf1e443
SumitNalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-08-17 13:48:56 -0500

    Delete script.js

--- static/script.js
+++
@@ -1,7 +0,0 @@
-function clear() {
-  for(let i of document.querySelectorAll(".response-text-container")) {
-    i.remove();
-  }
-}
-
-document.querySelector("#logo").addEventListener("click", (evt) => clear());
