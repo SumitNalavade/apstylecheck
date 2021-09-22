88affa577c64e1966dcedf227fabffbc98c5c59e
Sumit Nalavade <48499839+SumitNalavade@users.noreply.github.com>
2021-09-22 14:10:46 -0700

    Update apstylecheck.py

--- apstylecheck.py
+++ apstylecheck.py
@@ -15,11 +15,11 @@ def validate_wordisnum(text):
 
 corrections = {}
 
-first = {
-    "initial" : "",
-    "fixed" : "",
-    "index" : ""
-}
+class correction:
+    def __init__(self, initial, fixed, index):
+        self.initial = initial
+        self.fixed = fixed
+        self.index = index
 
 def check(text):
     corrections.clear()
@@ -38,61 +38,50 @@ def check(text):
         for index, i in enumerate(textArr):
             try:
                 if(i.isnumeric() == True and textArr.index(i) == 0):
-                    first.update({"initial" : i, "fixed" : word2number(i), "index" : index})
-                    corrections[i] = first
+                    corrections[i] = correction(i, word2number(i), index)
                 elif(validate_wordisnum(i) == True and textArr.index(i) == 0):
                     textArr.pop(textArr.index(i) + 1)
                 elif(i.isnumeric() == True and (textArr[textArr.index(i) + 1] == "years" or textArr[textArr.index(i) + 1] == "year")):
                     continue
                 elif(validate_wordisnum(i) == True and (textArr[textArr.index(i) + 1] == "years" or textArr[textArr.index(i) + 1] == "year")):
-                    first.update({"initial" : i, "fixed" : w2n.word_to_num(i), "index" : index})
-                    corrections[i] = first
+                    corrections[i] = correction(i, w2n.word_to_num(i), index)
                 elif(i.isnumeric() == True and (textArr[textArr.index(i) + 1] != "years" or textArr[textArr.index(i) + 1] != "year")):
                     if(int(i) in range (0,10) or (int(i) >= 100)):
                         if(textArr[textArr.index(i) + 1] != "percent"):
-                            first.update({"initial" : i, "fixed" : num2words(i), "index" : index})
-                            corrections[i] = first
+                            corrections[i] = correction(i, num2words(i), index)
                 elif(validate_wordisnum(i) == True and (textArr[textArr.index(i) + 1] != "years" or textArr[textArr.index(i) + 1] != "year")):
                         if(w2n.word_to_num(i) in range(10,100)):
-                            first.update({"initial" : i, "fixed" : w2n.word_to_num(i), "index" : index})
-                            corrections[i] = first
+                            corrections[i] = correction(i, w2n.word_to_num(i), index)
             except IndexError:
                 pass
 
         for index, i in enumerate(textArr):
             #Warn schools should be capitalized
             if("school" in i):
-                first.update({"initial" : i, "fixed" : "Make sure first letter of each word in school name is capitalized", "index" : index})
-                corrections[i] = first
+                corrections[i] = correction(i, "Make sure first letter of each word in school name is capitalized", index)
             
             if(i == "its" or i == "it’s" or i == "it's"): #Check for proper use of it's
-                first.update({"initial" : i, "fixed" : f"check for proper use of {i}", "index" : index})
-                corrections[i] = first
+                corrections[i] = correction(i, f"check for proper use of {i}", index)
 
             if("effect" in i or "affect" in i): #Check for proper use of effect/affect
-                first.update({"initial" : i, "fixed" : f"check for proper use of {i}", "index" : index})
-                corrections[i] = first
+                corrections[i] = correction(i, f"check for proper use of {i}", index)
 
             seasons = ["winter", "spring", "summer", "fall"] #Check seasons capitalization
             if(i in seasons):
-                first.update({"initial" : i, "fixed" : "Check for proper capitalizations of seasons. Seasons should not be capitalized unless used in a title", "index" : index})
-                corrections[i] = first
+                corrections[i] = correction(i, "Check for proper capitalizations of seasons. Seasons should not be capitalized unless used in a title", index)
 
             if("%" in i):
-                first.update({"initial" : i, "fixed" : "% should be spelled out (percent)", "index" : index})
-                corrections[i] = first
+                corrections[i] = correction(i, "% should be spelled out (percent)", index)
             try:
                 w2n.word_to_num(i)
             except ValueError:
                 pass
             else:
                 if((textArr.index(i) != 0 and i.isnumeric() == False) and textArr[textArr.index(i) + 1] == "percent"):
-                    first.update({"initial" : i, "fixed" : w2n.word_to_num(i), "index" : index})
-                    corrections[i] = first
+                    corrections[i] = correction(i, w2n.word_to_num(i), index)
 
             titles = ["senior","junior","sophomore","freshman","freshmen","principal","principle"]
             if(i in titles):
-                first.update({"initial" : i, "fixed" : "check for correct capitalization of title", "index" : index})
-                corrections[i] = first 
+                corrections[i] = correction(i, "check for correct capitalization of title", index) 
 
     return corrections
